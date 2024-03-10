190f6e3255a5200c2e5c303ab579aed1218a29ab
anas20023 <yorder07@gmail.com>
2024-03-10 09:28:34 +0600

    Update registration message and styles

--- public/timer.js
+++ public/timer.js
@@ -5,7 +5,6 @@ let secs = document.querySelector("#secs");
 const timer_div = document.getElementById("timer_div");
 
 document.addEventListener("DOMContentLoaded", function () {
-
   const targetDate = new Date("March 16, 2024 23:59:59").getTime();
 
   const countdownInterval = setInterval(updateCountdown, 1000);
@@ -14,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const currentDate = new Date().getTime();
 
     const remainingTime = targetDate - currentDate;
-
 
     const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
@@ -25,17 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
     );
     const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-
     dys.innerHTML = `${days}`;
     hrs.innerHTML = `${hours}`;
     mints.innerHTML = `${minutes}`;
     secs.innerHTML = `${seconds}`;
 
-
     if (remainingTime < 0) {
       timer_div.classList.remove("flex");
       timer_div.classList.add("hidden");
-      document.getElementById("Last_date").innerHTML = "Registration Time expired!";
+      document.getElementById("Last_date").innerHTML =
+        "Registration Time expired!";
+      document.getElementById("pre_msg").innerHTML = "";
+      document.getElementById("Deadline").innerHTML =
+        "Onsite Contest : 22-March-2024";
+      document.getElementById("Deadline").classList.remove("font-semibold");
+      document.getElementById("Deadline").classList.add("font-extrabold");
+      document.getElementById("register_btn").classList.add("hidden");
     }
   }
 });
